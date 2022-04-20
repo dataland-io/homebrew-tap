d47ea9cfac10a86d32bb55a2105150c4d4ce08b0
hzuo <someotherusername@gmail.com>
2022-04-20 16:25:04 -0400

    version 0.7.1

--- dataland-cli.rb
+++ dataland-cli.rb
@@ -4,11 +4,11 @@
 class DatalandCli < Formula
   desc "Dataland command-line interface"
   homepage "https://dataland.io"
-  version "0.6.0"
+  version "0.7.1"
 
   on_macos do
-    url "https://dl.stainless.dev/dataland-cli/releases/0.6.0/dataland-cli-0.6.0-x86_64-macos.tar.gz"
-    sha256 "0c4ec268c80abc3f32f89862d0a51406dd6e750bde5fea23bf5bbc3b081a2f77"
+    url "https://dl.stainless.dev/dataland-cli/releases/0.7.1/dataland-cli-0.7.1-x86_64-macos.tar.gz"
+    sha256 "7afbc042b9c07b18205fc2e7ee722ef72c41d660dd0b3d1a91ac6ddc0f880f2b"
 
     def install
       bin.install "dataland"
@@ -17,8 +17,8 @@ class DatalandCli < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://dl.stainless.dev/dataland-cli/releases/0.6.0/dataland-cli-0.6.0-x86_64-linux.tar.gz"
-      sha256 "d365ce5ba68f202b4492f3d7f161a65bb569cb5a4974c5d4b27e22249d22d541"
+      url "https://dl.stainless.dev/dataland-cli/releases/0.7.1/dataland-cli-0.7.1-x86_64-linux.tar.gz"
+      sha256 "93a863f2e6d275993f9a40b65dac077391f02194bcde9992b1d96f2ef7330154"
 
       def install
         bin.install "dataland"
